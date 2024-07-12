627267e3ed858749ffefb4e56610bbe6d0150ec1
Trung Dang <29.11.2003.trung@gmail.com>
2024-07-12 22:05:20 +0700

    GET request here need not polling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dotenv import find_dotenv, load_dotenv
 from os import environ
-from time import sleep
 import requests
 import logging
 import json
@@ -19,32 +18,35 @@ news_api_key = environ.get("NEWS_API_KEY")
 
 def get_news(topic):
     url = (
-        f"https://newsapi.org/v2/everything?q={topic}&apiKey={news_api_key}&pageSize=5"  # pageSize determines the number of articles to be received
+        f"https://newsapi.org/v2/everything?q={topic}&apiKey={news_api_key}&pageSize=5"
+        # pageSize determines the number of articles to be received
     )
     news = list()
-    sleep_interval = 1
 
-    while True:
-        try:
-            response = requests.get(url)  # 'response' is now a JSON object
-            if response.status_code == 200:
-                response = response.json()  # 'response' is now a Python object
-                response = json.dumps(response, indent=4)  # 'response is now a JSON formatted string
-                response = json.loads(response)  # 'response' is now a Python formatted object, usually dictionary
+    try:
+        response = requests.get(url)  # 'response' is now a JSON object
+                                      # without parameter 'timeout=', GET request will continue until the connection is closed
+                                      # without parameter 'timeout=', GET request will run until it gets a status code,
+                                      # and stop when it gets a status code, regardless of what the code is
+                                      # so no need polling in this use case
+        if response.status_code == 200:
+            response = response.json()  # 'response' is now a Python object
+            response = json.dumps(response, indent=4)  # 'response is now a JSON formatted string
+            response = json.loads(response)  # 'response' is now a Python formatted object, usually dictionary
 
-                status = response["status"]
-                articles = response["articles"]
-                number = 0
+            status = response["status"]
+            articles = response["articles"]
+            number = 0
 
-                if status == "ok":
-                    for article in articles:
-                        source_name = article["source"]["name"]
-                        author = article["author"]
-                        title = article["title"]
-                        description = article["description"]
-                        url = article["url"]
-                        number += 1
-                        layout = f"""
+            if status == "ok":
+                for article in articles:
+                    source_name = article["source"]["name"]
+                    author = article["author"]
+                    title = article["title"]
+                    description = article["description"]
+                    url = article["url"]
+                    number += 1
+                    layout = f"""
 {number}
 Title: {title}
 Author: {author}
@@ -52,23 +54,24 @@ Source: {source_name}
 Description: {description}
 URL: {url}
 """
-                        news.append(layout)
-                    
-                    logging.info("Response received successfully")
-                    return news
-                    break  # this 'break' doesn't run because a function ends at 'return'
+                    news.append(layout)
+                
+                logging.info("Response received successfully")
+                return news
 
-                else:
-                    logging.info("Reponse received but incorrect")
-                    break
+            else:
+                logging.info("Reponse received but incorrect")
+                return []
+        
+        else:
+            logging.info("Request unsuccessful")
+            return []
 
-            logging.info("Waiting for response...")
-            sleep(sleep_interval)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error occurred during API request: {e}")
+        return []
 
-        except requests.exceptions.RequestException as e:
-            logging.error(f"Error occurred during API request: {e}")
-            break
-            
+
 topic = input("Enter your topic of interest: ")
 news = get_news(topic)
 for new in news:
